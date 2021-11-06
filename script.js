5534c3e2d92629237c098fad1caf511a0972e7a6
Popoola Seyitan <60761852+Watermelon-Sugar@users.noreply.github.com>
2021-11-06 23:00:50 +0100

    first commmit

---
+++ script.js
@@ -0,0 +1,15 @@
+const images = document.querySelectorAll('.panel');
+
+images.forEach(image => {
+    image.addEventListener('click', () => {
+        removeActiveClasses()
+        image.classList.add('active')
+    })
+})
+
+
+function removeActiveClasses() {
+    images.forEach(image => {
+        image.classList.remove('active')
+    })
+}
